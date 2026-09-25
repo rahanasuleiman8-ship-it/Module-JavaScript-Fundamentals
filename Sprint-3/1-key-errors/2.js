307d6b9a3afe736168a076df5ec5c6039c06ad71
Rahana Suleiman <rahanasuleiman8@gmail.com>
2026-09-25 10:51:29 +0100

    - Replace literal number 3 with parameter identifier num - resolve parameter syntax error in square function

--- Sprint-3/1-key-errors/2.js
+++ Sprint-3/1-key-errors/2.js
@@ -1,20 +1,31 @@
 
 // Predict and explain first BEFORE you run any code...
+// Prediction: We will get an error definitely but i'm not sure if it's going to be a ReferenceError or a SyntaxError
 
 // this function should square any number but instead we're going to get an error
 
 // =============> write your prediction of the error here
 
-function square(3) {
-    return num * num;
-}
+// function square(3) {
+//     return num * num;
+// }
+
+// console.log(square(2));
 
 // =============> write the error message here
 
+// =============> ERROR MESSAGE: SyntaxError: Unexpected number
+
 // =============> explain this error message here
+
+// =============> EXPLANATION: The SyntaxError: Unexpected number happened because the function was expecting an identifier in the  parameter ('num') and not a number ('3') which triggered the error 
 
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
 
+function square(num) {
+    return num * num;
+}
 
+console.log(square(2));
