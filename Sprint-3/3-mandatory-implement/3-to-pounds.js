7c3a6c5de1283c83e3ca0d924023f0b8a1a01129
Rahana Suleiman <rahanasuleiman8@gmail.com>
2026-09-26 15:57:20 +0100

    Implement toPence function to convert pence strings to formatted pounds

--- Sprint-3/3-mandatory-implement/3-to-pounds.js
+++ Sprint-3/3-mandatory-implement/3-to-pounds.js
@@ -4,3 +4,30 @@
 // You will need to declare a function called toPounds with an appropriately named parameter.
 
 // You should call this function a number of times to check it works for different inputs
+
+
+
+function toPence(penceString) {
+
+const penceStringWithoutTrailingP = penceString.substring(
+  0,
+  penceString.length - 1
+);
+
+const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
+const pounds = paddedPenceNumberString.substring(
+  0,
+  paddedPenceNumberString.length - 2
+);
+
+const pence = paddedPenceNumberString
+  .substring(paddedPenceNumberString.length - 2)
+  .padEnd(2, "0");
+
+ return (`£${pounds}.${pence}`)
+};
+
+console.log(toPence('399p')); // output £3.99
+console.log(toPence('50p')); // output £0.50
+console.log(toPence('1200p')); // output £12.00
+console.log(toPence('5p')); // output £0.05
